76f43e65f78c1b9833865dad057a8e7f10ea2e3f
pedroamas <pedromassj@gmail.com>
2023-04-10 18:05:29 -0300

    lo solucioneee

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,9 +19,9 @@ function Header(props)  {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="/">To-do list</Nav.Link>
-              <Nav.Link href="/chess">Chess</Nav.Link>
-              <Nav.Link href="/minesweeper">Minesweeper</Nav.Link>
-              <Nav.Link href="/snake">Snake</Nav.Link>
+              <Nav.Link href="/#/chess">Chess</Nav.Link>
+              <Nav.Link href="/#/minesweeper">Minesweeper</Nav.Link>
+              <Nav.Link href="/#/snake">Snake</Nav.Link>
               {/* <Nav.Link href="/freecell">FreeCell</Nav.Link> */}
               {/* <Nav.Link href="/streaming">Streaming</Nav.Link> */}
               {/* <Nav.Link href="/about">About</Nav.Link> */}
